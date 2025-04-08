daccfd92ac66bce912b5af96a72a4861cbaf9bf5
waffleleroo <waffleleroo@gmail.com>
2025-04-07 21:50:57 -0700

    - Added a bunch of egui - Made debug settings better in general - Removed a few keybinds - Fixed scrolling (again) - Made zooming and key-based movement possible in play state - Added WASD - Added grid configuration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use avian2d::{math::*, prelude::*};
-use bevy::{
-    input::mouse::MouseMotion,
-    prelude::*,
+use bevy::{input::mouse::MouseMotion, prelude::*};
+use bevy_egui::{
+    EguiContexts, EguiPlugin,
+    egui::{self},
 };
 
 const GRAVITATIONAL_CONSTANT: f32 = 1.0;
-const CAMERA_MOVE_SPEED: f32 = 5.0;
+const CAMERA_MOVE_SPEED: f32 = 10.0;
 
 #[derive(Component, Debug)]
 struct Mass(f32);
@@ -17,7 +18,37 @@ struct Gravitable;
 struct Gravitator;
 
 #[derive(Resource)]
-struct DebugInfo(bool);
+struct DebugSettings {
+    show_grid: bool,
+    show_velocity_arrows: bool,
+    show_grid_settings: bool,
+    grid_settings: GridSettings,
+}
+
+struct GridSettings {
+    // The size of each cell
+    cell_size: Vec2,
+    // How many cells to draw on x and y axis
+    grid_draw_dimensions: Vec2,
+    // Size of the upper lines
+    recursive_depth: i32,
+    // Color of lower lines
+    lower_color: Hsva,
+    // Color of upper lines (Hsva because of egui)
+    upper_color: Hsva,
+}
+
+impl Default for GridSettings {
+    fn default() -> Self {
+        GridSettings {
+            cell_size: Vec2::new(64.0, 64.0),
+            grid_draw_dimensions: Vec2::new(16.0, 16.0),
+            recursive_depth: 2,
+            lower_color: Hsva::new(0.0, 0.0, 0.2, 1.0),
+            upper_color: Hsva::new(0.0, 0.0, 0.45, 1.0),
+        }
+    }
+}
 
 #[derive(Default, Reflect, GizmoConfigGroup)]
 struct ArrowGizmos;
@@ -72,7 +103,7 @@ enum GameState {
 fn main() {
     App::new()
         .init_gizmo_group::<ArrowGizmos>()
-        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default(), EguiPlugin))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
@@ -81,7 +112,6 @@ fn main() {
                 mouse_tracker_sys,
                 draw_velocity_arrows,
                 mouse_input,
-                toggle_arrows,
                 toggle_gamestate,
                 release_object,
                 draw_selected_velocity_arrows,
@@ -92,6 +122,8 @@ fn main() {
                 mouse_panning,
                 move_camera_around_main_object,
                 draw_grid,
+                simulation_setting,
+                grid_settings,
             ),
         )
         .add_systems(PostUpdate, (clear_level, reset_level))
@@ -105,22 +137,18 @@ fn setup(
 ) {
     commands.spawn((Camera2d, GameCamera));
 
-    commands.insert_resource(DebugInfo(true));
+    commands.insert_resource(DebugSettings {
+        show_grid: false,
+        show_velocity_arrows: false,
+        show_grid_settings: false,
+        grid_settings: GridSettings::default(),
+    });
     commands.insert_resource(MousePos(Vec2::ZERO));
     commands.insert_resource(GameState::Editor);
 
     // Disable Avian Gravity
     commands.insert_resource(Gravity::ZERO);
 }
-
-fn toggle_arrows(keys: Res<ButtonInput<KeyCode>>, mut show_arrows: ResMut<DebugInfo>) {
-    if keys.just_pressed(KeyCode::KeyQ) {
-        show_arrows.0 = !show_arrows.0
-    }
-}
-
-// I still don't know if seperating inputs like this is OK
-// Should run in parralel(?)
 
 fn toggle_gamestate(keys: Res<ButtonInput<KeyCode>>, mut state: ResMut<GameState>) {
     if keys.just_pressed(KeyCode::KeyE) {
@@ -187,10 +215,7 @@ fn move_camera_around_main_object(
 
             // Move 1/60th of the distance towards the obj
 
-            update_xy(
-                &mut camera_transform.translation,
-                dbg!(-dist_between / 60.0),
-            );
+            update_xy(&mut camera_transform.translation, -dist_between / 60.0);
         } else {
             // Outside of bounding box
 
@@ -200,8 +225,6 @@ fn move_camera_around_main_object(
 
             update_xy(&mut camera_transform.translation, dist_between);
         }
-
-        println!("Camera translation {camera_transform:?}");
     }
 }
 
@@ -213,66 +236,61 @@ fn update_xy(vec: &mut Vec3, xy: Vec2) {
 fn move_camera(
     mut camera_query: Query<(&mut Transform, &OrthographicProjection), With<GameCamera>>,
     keys: Res<ButtonInput<KeyCode>>,
-    state: Res<GameState>,
 ) {
-    if *state == GameState::Editor {
-        let (mut transform, projection) = camera_query.single_mut();
+    use KeyCode::*;
 
-        let camera_movement_speed = CAMERA_MOVE_SPEED * projection.scale;
+    let (mut transform, projection) = camera_query.single_mut();
 
-        if keys.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += camera_movement_speed;
-        }
+    let camera_movement_speed = CAMERA_MOVE_SPEED * projection.scale;
 
-        if keys.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= camera_movement_speed;
-        }
+    if keys.any_pressed([ArrowRight, KeyD]) {
+        transform.translation.x += camera_movement_speed;
+    }
 
-        if keys.pressed(KeyCode::ArrowDown) {
-            transform.translation.y -= camera_movement_speed;
-        }
+    if keys.any_pressed([ArrowLeft, KeyA]) {
+        transform.translation.x -= camera_movement_speed;
+    }
 
-        if keys.pressed(KeyCode::ArrowUp) {
-            transform.translation.y += camera_movement_speed;
-        }
+    if keys.any_pressed([ArrowDown, KeyS]) {
+        transform.translation.y -= camera_movement_speed;
+    }
+
+    if keys.any_pressed([ArrowUp, KeyW]) {
+        transform.translation.y += camera_movement_speed;
     }
 }
 
 fn zoom_camera(
     mut camera_query: Query<(&mut OrthographicProjection, &mut Transform), With<GameCamera>>,
     mut scroll_events: EventReader<bevy::input::mouse::MouseWheel>,
-    state: Res<GameState>,
     mouse_pos: Res<MousePos>,
 ) {
     use bevy::input::mouse::MouseScrollUnit;
 
-    if *state == GameState::Editor {
-        let (mut projection, mut transform) = camera_query.single_mut();
+    let (mut projection, mut transform) = camera_query.single_mut();
 
-        for event in scroll_events.read() {
-            let mut zoom_modifier = 1.0;
+    for event in scroll_events.read() {
+        let mut zoom_modifier = 1.0;
 
-            match event.unit {
-                MouseScrollUnit::Line => {
-                    if event.y <= -1.0 {
-                        projection.scale *= 1.1;
-                        zoom_modifier = -1.0;
-                    } else if event.y >= 1.0 {
-                        projection.scale /= 1.1;
-                    }
-
-                    let dif_vec = -transform.translation
-                        + Vec3::new(mouse_pos.0.x, mouse_pos.0.y, 0.0) * zoom_modifier;
-
-                    transform.translation += dif_vec / 10.0;
+        match event.unit {
+            MouseScrollUnit::Line => {
+                if event.y <= -1.0 {
+                    projection.scale *= 1.1;
+                    zoom_modifier = -1.1;
+                } else if event.y >= 1.0 {
+                    projection.scale /= 1.1;
                 }
-                MouseScrollUnit::Pixel => {
-                    println!(
-                        "Scroll (pixel units): vertical: {}, horizontal: {}",
-                        event.y, event.x
-                    );
-                    todo!()
-                }
+
+                let dif_vec = -transform.translation + Vec3::new(mouse_pos.0.x, mouse_pos.0.y, 0.0); // * zoom_modifier;
+
+                transform.translation += (dif_vec / 10.0) * zoom_modifier;
+            }
+            MouseScrollUnit::Pixel => {
+                println!(
+                    "Scroll (pixel units): vertical: {}, horizontal: {}",
+                    event.y, event.x
+                );
+                todo!()
             }
         }
     }
@@ -485,11 +503,11 @@ fn create_static(
 }
 
 fn draw_velocity_arrows(
-    show_arrows: Res<DebugInfo>,
+    debug_settings: Res<DebugSettings>,
     mut gizmos: Gizmos,
     query: Query<(&LinearVelocity, &Transform)>,
 ) {
-    if show_arrows.0 {
+    if debug_settings.show_velocity_arrows {
         query.iter().for_each(|(velocity, transform)| {
             gizmos.arrow_2d(
                 transform.translation.xy(),
@@ -501,12 +519,12 @@ fn draw_velocity_arrows(
 }
 
 fn draw_selected_velocity_arrows(
-    debug_info: Res<DebugInfo>,
+    debug_info: Res<DebugSettings>,
     mouse_pos: Res<MousePos>,
     mut gizmos: Gizmos,
     transform_query: Query<&Transform, With<Selected>>,
 ) {
-    if debug_info.0 {
+    if debug_info.show_velocity_arrows {
         transform_query.iter().for_each(|transform| {
             let dif = -(mouse_pos.0 - transform.translation.xy());
 
@@ -520,16 +538,20 @@ fn draw_selected_velocity_arrows(
 }
 
 fn draw_grid(
-    debug_info: Res<DebugInfo>,
+    debug_settings: Res<DebugSettings>,
     mut gizmos: Gizmos,
     camera_query: Query<(&Transform, &OrthographicProjection), With<GameCamera>>,
 ) {
-    if debug_info.0 {
+    if debug_settings.show_grid {
         let (camera_transform, projection) = camera_query.single();
 
         let mut scale = projection.scale;
 
-        let scale_jumps_iter = std::iter::successors(Some(0.00390625_f32), |x| Some(x * 2.0));
+        let grid_settings = &debug_settings.grid_settings;
+        let recursive_depth: f32 = grid_settings.recursive_depth as f32;
+
+        let scale_jumps_iter =
+            std::iter::successors(Some(0.00390625_f32), |x| Some(x * recursive_depth));
 
         for scale_jump in scale_jumps_iter {
             if scale_jump > scale {
@@ -538,43 +560,117 @@ fn draw_grid(
             }
         }
 
-        let grid_spacing = scale * 64.0;
+        let grid_spacing = scale * grid_settings.cell_size;
 
         let camera_xy = camera_transform.translation.xy();
 
-        let closest_center = dbg!(camera_xy - (camera_xy % (grid_spacing * 2.0)));
+        let closest_center = camera_xy - (camera_xy % (grid_spacing * recursive_depth));
 
-        let x_width = 16_f32;
-        let y_width = 16_f32;
+        let x_width = grid_settings.grid_draw_dimensions.x;
+        let y_width = grid_settings.grid_draw_dimensions.y;
 
         for x in -x_width as i32..x_width as i32 {
-            let line_x: f32 = x as f32 * grid_spacing;
-            let lightness = if dbg!(line_x % (grid_spacing * 2.0)) == 0.0 {
-                0.2
+            let line_x: f32 = x as f32 * grid_spacing.x;
+            let color = if line_x % (grid_spacing.x * recursive_depth) == 0.0 {
+                grid_settings.upper_color
             } else {
-                0.05
+                grid_settings.lower_color
             };
             gizmos.line_2d(
-                Vec2::new(line_x as f32, y_width * grid_spacing) + closest_center,
-                Vec2::new(line_x as f32, -y_width * grid_spacing) + closest_center,
-                LinearRgba::gray(lightness),
+                Vec2::new(line_x as f32, y_width * grid_spacing.y) + closest_center,
+                Vec2::new(line_x as f32, -y_width * grid_spacing.y) + closest_center,
+                color,
             );
         }
 
         for y in -y_width as i32..y_width as i32 {
-            let line_y: f32 = y as f32 * grid_spacing;
+            let line_y: f32 = y as f32 * grid_spacing.y;
 
-            let lightness = if line_y % (grid_spacing * 2.0) == 0.0 {
-                0.2
+            let color = if line_y % (grid_spacing.y * recursive_depth) == 0.0 {
+                grid_settings.upper_color
             } else {
-                0.05
+                grid_settings.lower_color
             };
 
             gizmos.line_2d(
-                Vec2::new(x_width * grid_spacing, line_y as f32) + closest_center,
-                Vec2::new(-x_width * grid_spacing, line_y as f32) + closest_center,
-                LinearRgba::gray(lightness),
+                Vec2::new(x_width * grid_spacing.x, line_y as f32) + closest_center,
+                Vec2::new(-x_width * grid_spacing.x, line_y as f32) + closest_center,
+                color,
             );
         }
     }
+}
+
+fn simulation_setting(
+    mut contexts: EguiContexts,
+    state: Res<GameState>,
+    debug_settings: ResMut<DebugSettings>,
+) {
+    if *state == GameState::Editor {
+        let mut debug_settings = debug_settings;
+
+        egui::Window::new("Editor Mode").show(contexts.ctx_mut(), |ui| {
+            ui.checkbox(&mut debug_settings.show_grid, "Show grid");
+            ui.checkbox(
+                &mut debug_settings.show_velocity_arrows,
+                "Show velocity arrows",
+            );
+            ui.checkbox(&mut debug_settings.show_grid_settings, "Show grid settings")
+        });
+    }
+}
+
+fn grid_settings(
+    mut contexts: EguiContexts,
+    state: Res<GameState>,
+    debug_settings: ResMut<DebugSettings>,
+) {
+    let mut debug_settings = debug_settings;
+
+    if *state == GameState::Editor && debug_settings.show_grid_settings {
+        let grid_settings = &mut debug_settings.grid_settings;
+        let mut lower_egui_hsva = bevy_hsva_to_egui_hsva(grid_settings.lower_color);
+        let mut upper_egui_hsva = bevy_hsva_to_egui_hsva(grid_settings.upper_color);
+
+        egui::Window::new("Grid Settings").show(contexts.ctx_mut(), |ui| {
+            ui.label("Cell Dimensions");
+            ui.horizontal(|ui| {
+                ui.label("x");
+                ui.add(egui::DragValue::new(&mut grid_settings.cell_size.x));
+                ui.label("y");
+                ui.add(egui::DragValue::new(&mut grid_settings.cell_size.y))
+            });
+            ui.label("Cell Draw Dimensions");
+            ui.horizontal(|ui| {
+                ui.label("x");
+                ui.add(egui::DragValue::new(
+                    &mut grid_settings.grid_draw_dimensions.x,
+                ));
+                ui.label("y");
+                ui.add(egui::DragValue::new(
+                    &mut grid_settings.grid_draw_dimensions.y,
+                ))
+            });
+            ui.label("Recursion count");
+            ui.add(egui::Slider::new(&mut grid_settings.recursive_depth, 2..=6));
+            ui.label("Colors");
+            ui.horizontal(|ui| {
+                ui.label("Lower");
+                ui.color_edit_button_hsva(&mut lower_egui_hsva);
+                ui.label("Upper");
+                ui.color_edit_button_hsva(&mut upper_egui_hsva);
+            })
+        });
+
+        grid_settings.lower_color = egui_hsva_to_bevy_hsva(lower_egui_hsva);
+        grid_settings.upper_color = egui_hsva_to_bevy_hsva(upper_egui_hsva);
+    }
+}
+
+fn egui_hsva_to_bevy_hsva(hsva: egui::epaint::Hsva) -> Hsva {
+    Hsva::new(hsva.h, hsva.s, hsva.v, hsva.a)
+}
+
+fn bevy_hsva_to_egui_hsva(hsva: Hsva) -> egui::epaint::Hsva {
+    egui::epaint::Hsva::new(hsva.hue, hsva.saturation, hsva.value, hsva.alpha)
 }
